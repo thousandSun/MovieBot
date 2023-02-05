5a84fd98c920d3b8cccbf3d4638927b7ecefa466
Filiberto Rios <filibertoerios@gmail.com>
2023-02-05 12:02:25 -0600

    basic poll created; needs db connection for movie names

--- events/ready.js
+++ events/ready.js
@@ -1,15 +1,43 @@
 const { Events, EmbedBuilder } = require('discord.js');
-const cron = require('cron').CronJob
+const cron = require('cron').CronJob;
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
-    execute(client){
+    async execute(client) {
+
+        const guild = await client.guilds.cache.get('1070448397653397585');
+        if (guild) {
+            const channel = guild.channels.cache.get('1070448550955208784');
+
+            // TODO:
+            // get 4 random unwatched movies from the db
+            // replace `Movie n` with movie name
+
+            const pollEmbed = new EmbedBuilder()
+                .setColor(0xE05263)
+                .setTitle('Movie Night Poll')
+                .setAuthor({ name: client.user.username})
+                .setDescription('React with the associated emote to vote for the movie to watch tonight.')
+                .addFields({ name: 'Movie 1', value: ':one:', inline:true }, { name: 'Movie 2', value: ':two:', inline:true })
+                .addFields({ name: '\u200B', value: '\u200B' })
+                .addFields({ name: 'Movie 3', value: ':three:', inline:true }, { name: 'Movie 4', value: ':four:', inline:true })
+                .setTimestamp()
+                .setFooter({ text: 'Thank you for participating in this poll. See you next week!' });
+
+            const message = await channel.send({ content: ` It's Friday, that means movie poll time!`, embeds: [pollEmbed], fetchReply: true });
+            const reactions = [ '1️⃣', '2️⃣', '3️⃣', '4️⃣'];
+            const reaction = reactions[Math.floor(Math.random() * reactions.length)];
+            console.log(reaction);
+            message.react(reaction);
+        }
+
+
         // const sendFridayPoll = new cron('0 17 * * 5', async function(){
         //     const guild = client.guilds.cache.get('1070448397653397585');
         //     const everyone = guild.roles.fetch('@everyone');
         //     if(guild){
-        //         const channel = guild.channels.cache.get('1071122301426339850');
+        //         const channel = guild.channels.cache.get('1070448550955208784');
 
         //         // TODO:
         //         // get 4 random unwatched movies from the db
